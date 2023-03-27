c3f77949361d88d479ad8e5bf02d81d799477856
Kinexity <26kuba05@gmail.com>
2023-03-27 23:19:06 +0200

    new files who dis

---
+++ src/OTPCRunAction3.cc
@@ -0,0 +1,82 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: ExN02RunAction.cc,v 1.3 1999/12/15 14:49:22 gunter Exp $
+// GEANT4 tag $Name: geant4-02-00 $
+//
+////////////////////////////////////////////////////////////////
+//
+//  V. Guadilla 2021
+///////////////////////////////////////////////////////////////
+
+#include "OTPCRunAction.hh"
+
+#include "G4Run.hh"
+
+#include "G4ios.hh"
+#include "fstream"
+#include "iomanip"
+#include "Randomize.hh"
+#include "time.h"
+
+
+#include "G4SystemOfUnits.hh"
+// #include "g4std/fstream"
+// #include "g4std/iomanip"
+
+using namespace std;
+
+
+OTPCRunAction::OTPCRunAction()
+{
+	timer = new G4Timer;
+
+	eventFile.open("event.csv", std::ios_base::out);
+
+	///////////////////////////////////////////////////////////////////////////////////	
+
+}
+
+OTPCRunAction::~OTPCRunAction()
+{
+	delete timer;
+}
+
+void OTPCRunAction::BeginOfRunAction(const G4Run*)
+{
+	//Start CPU timer
+	timer->Start();
+
+}
+
+void OTPCRunAction::EndOfRunAction(const G4Run*)
+{
+
+	eventFile.close();
+
+	//Stop timer and get CPU time
+	timer->Stop();
+	G4double cputime = timer->GetUserElapsed();
+	cout << " CPU time = " << cputime << " s" << endl;
+
+
+}
+
+void OTPCRunAction::fillOut(std::vector<std::array<G4double, 4>> EnergyDeposit, std::array<G4double, 20> EnergyGammaCrystals)
+{
+
+	for (auto& EnergyDeposit_i : EnergyDeposit) {
+
+		if (EnergyDeposit_i[3] > 0) {
+			eventFile << EnergyDeposit_i[0] << ',' << EnergyDeposit_i[1] << ',' << EnergyDeposit_i[2] << ',' << EnergyDeposit_i[3] << '\n';
+		}
+	}
+
+	for (auto& EnergyDepositOneCrystal : EnergyGammaCrystals) {
+		eventFile << EnergyDepositOneCrystal << '\t';
+	}
+}
---
+++ src/OTPCRunAction3.hh
@@ -0,0 +1,67 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: ExN02RunAction.hh,v 1.3 1999/12/15 14:49:20 gunter Exp $
+// GEANT4 tag $Name: geant4-02-00 $
+//
+//
+/////////////////////////////////////////////////////////////////
+//
+//  V Guadilla 2021
+//
+/////////////////////////////////////////////////////////////////
+
+#ifndef OTPCRunAction_h
+#define OTPCRunAction_h 1
+
+#include "G4UserRunAction.hh"
+#include "globals.hh"
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <array>
+#include "G4Timer.hh"
+
+class G4Run;
+
+class G4Timer;
+
+class OTPCRunAction : public G4UserRunAction
+{
+  public:
+    OTPCRunAction();
+    ~OTPCRunAction();
+
+  public:
+    void BeginOfRunAction(const G4Run*);
+    void EndOfRunAction(const G4Run*);
+
+
+    void fillOut(std::vector<std::array<G4double, 4>> EnergyDeposit, std::array<G4double, 20> EnergyGammaCrystals);
+    
+    
+    
+private:
+   
+    G4Timer* timer;
+    
+    std::fstream eventFile;
+    G4double                  x[5000];
+    G4double                  y[5000];
+    G4double                  z[5000];
+    G4double                  E[5000];
+   
+};
+
+#endif
+
+
+
+
+
+
